c67e270bff50e3bbcd7ef35cc9dad65e6f9cd1fc
Vicky Yin <v-vyin@microsoft.com>
2020-08-19 13:59:06 +0800

    first commit

---
+++ DBtoExcel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBtoExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
